1e4919983aa96a3ed17cc931c924c7a945feb79b
tjyy <1123351509@qq.com>
2022-04-01 11:10:19 +0800

    使用正确的GPU计时方法 解决了 云边端计算时延不同的缺点 服务端应该加个循环 然后将功能封装一下 总用时还不准确

--- 毕设3_中间变量传输/04_alexnet_client.py
+++ 毕设3_中间变量传输/04_alexnet_client.py
@@ -1,4 +1,5 @@
 import a0_alexNet
+import function
 import socket_function
 import torch
 import time
@@ -13,24 +14,41 @@ AlexNet = a0_alexNet.AlexNet(input_layer = 3,num_classes = 1000)
 AlexNet = AlexNet.to(device)
 
 
-x = torch.rand(size=(2000,3,224,224),requires_grad=False)
+x = torch.rand(size=(128,3,224,224),requires_grad=False)
 x = x.to(device)
 print(f"x device : {x.device}")
 x.requires_grad = False
-time.sleep(2)
 
+# GPU warm-up and prevent it from going into power-saving mode
+dummy_input = torch.rand(x.shape).to(device)
+
+# init loggers
+starter = torch.cuda.Event(enable_timing=True)
+ender = torch.cuda.Event(enable_timing=True)
+
+# GPU warm-up
+with torch.no_grad():
+    for i in range(3):
+        _ = AlexNet(dummy_input)
+        print(f"GPU warm-up - {i+1}")
 
 for i in range(len(AlexNet) + 1):
     point_index = i
-    edge_model, cloud_model = a0_alexNet.model_partition(AlexNet, point_index)
+    edge_model, cloud_model = function.model_partition(AlexNet, point_index)
 
+    start_alltime = time.time()
 
-    start_alltime = int(round(time.time() * 1000))
-    start_time = int(round(time.time() * 1000))
+    # 开始记录时间
+    starter.record()
     with torch.no_grad():
         edge_x = edge_model(x)
-    end_time = int(round(time.time() * 1000))
-    print(f"从第{point_index}层进行划分\t边缘端计算用时 : {(end_time - start_time) / 1000 :>3} s")
+    ender.record()
+
+    # wait for GPU SYNC
+    torch.cuda.synchronize()
+    curr_time = starter.elapsed_time(ender)
+
+    print(f"从第{point_index}层进行划分\t边缘端计算用时 : {curr_time :.3f} ms")
 
     # 创建socket
     p = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -38,14 +56,12 @@ for i in range(len(AlexNet) + 1):
     p.connect(('127.0.0.1', 8080))
     edge_x = pickle.dumps(edge_x)
 
-
-
-    start_time = int(round(time.time() * 1000))
+    start_time = time.time()
     p.sendall(edge_x)
     # 收到第一次信号 说明已经接收到了传过去的edge_x数据
     data = p.recv(1024)
     # print(data)
-    end_time = int(round(time.time() * 1000))
+    end_time = time.time()
 
     """
         通过查看 starttime 和 endtime 的值
@@ -53,14 +69,15 @@ for i in range(len(AlexNet) + 1):
     """
     # print(start_time)
     # print(end_time)
-    print(f"传输时延 : {(end_time - start_time) / 1000 :>3} s")
+    print(f"传输时延 : {(end_time - start_time)*1000:.3f} ms")
 
 
     # 收到第二次信号 说明对面的云端数据已经计算完毕
     data2 = p.recv(1024)
     # print(data)
     # print("get the second yes,the entire computation has completed",data2)
-    end_alltime = int(round(time.time() * 1000))
-    print(f"从第{point_index}层进行划分\t云边协同计算用时 : {(end_alltime - start_alltime) / 1000 :>3} s")
+    end_alltime = time.time()
+
+    print(f"从第{point_index}层进行划分\t云边协同计算用时 : {(end_alltime - start_alltime)*1000:.3f} ms")
     print("====================================")
     p.close()
--- 毕设3_中间变量传输/04_alexnet_server.py
+++ 毕设3_中间变量传输/04_alexnet_server.py
@@ -12,20 +12,38 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 AlexNet = a0_alexNet.AlexNet(input_layer = 3,num_classes = 1000)
 AlexNet = AlexNet.to(device)
 
+x = torch.rand(size=(128,3,224,224),requires_grad=False)
+x = x.to(device)
+print(f"x device : {x.device}")
+x.requires_grad = False
+
+# GPU warm-up and prevent it from going into power-saving mode
+dummy_input = torch.rand(x.shape).to(device)
+
+# init loggers
+starter = torch.cuda.Event(enable_timing=True)
+ender = torch.cuda.Event(enable_timing=True)
+
+# GPU warm-up
+with torch.no_grad():
+    for i in range(3):
+        _ = AlexNet(dummy_input)
+        print(f"GPU warm-up - {i+1}")
+
+
 # 初始化一个 socket连接
 # 创建socket
 p = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-p.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+# p.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+p.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # 绑定端口号
 p.bind(('127.0.0.1', 8080))
 # 打开监听
 p.listen(5)
 
-
 for i in range(len(AlexNet) + 1):
     point_index = i
     edge_model, cloud_model = a0_alexNet.model_partition(AlexNet, point_index)
-
 
     # 等待客户端链接
     conn, client = p.accept()
@@ -33,10 +51,11 @@ for i in range(len(AlexNet) + 1):
     # 收发消息
     data = []
     idx = 0
+    start_time = 0
     while True:
         packet = conn.recv(1024)
-        if(idx == 0):
-            start_time = int(round(time.time() * 1000))
+        if idx == 0:
+            start_time = time.time()
         data.append(packet)
         # 100mb 提醒一次
         # if idx % (1024 * 100) == 0:
@@ -44,7 +63,7 @@ for i in range(len(AlexNet) + 1):
         idx += 1
         if len(packet) < 1024: break
     parse_data = pickle.loads(b"".join(data))
-    end_time = int(round(time.time() * 1000))
+    end_time = time.time()
 
 
     """
@@ -53,7 +72,7 @@ for i in range(len(AlexNet) + 1):
     """
     # print(start_time)
     # print(end_time)
-    print(f"传输时延: {(end_time - start_time) / 1000 :>3}s")
+    print(f"传输时延: {(end_time - start_time)*1000:.3f} ms")
     print(f"client data: {parse_data.shape}")
     parse_data.requires_grad = False
 
@@ -61,12 +80,17 @@ for i in range(len(AlexNet) + 1):
     # print("get the client's data,return yes to client",data1)
     conn.sendall(data1)
 
-    start_time2 = int(round(time.time() * 1000))
+    # 开始记录时间
+    starter.record()
     with torch.no_grad():
         cloud_x = cloud_model(parse_data)
-    end_time2 = int(round(time.time() * 1000))
+    ender.record()
 
-    print(f"从第{point_index}层进行划分\t云端计算用时 : {(end_time2 - start_time2) / 1000 :>3} s")
+    # wait for GPU SYNC
+    torch.cuda.synchronize()
+    curr_time = starter.elapsed_time(ender)
+
+    print(f"从第{point_index}层进行划分\t云端计算用时 : {curr_time :.3f} ms")
     print("==============================================")
 
     conn.sendall("yes".encode("UTF-8"))
