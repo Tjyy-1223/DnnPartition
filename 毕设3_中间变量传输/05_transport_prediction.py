992478a5c8d4174edf481f0295ebfde6705e7e01
tjyy <1123351509@qq.com>
2022-04-16 15:33:21 +0800

    rename img and extract function

--- 毕设3_中间变量传输/05_transport_prediction.py
+++ 毕设3_中间变量传输/05_transport_prediction.py
@@ -6,6 +6,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def getScatterImg(x,y):
+    s = 50
+
+    fig = plt.figure(figsize=(8, 5))
+
+    plt.scatter(x, y, s, c="g", alpha=0.5)
+    plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0), useMathText=True)
+    plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0), useMathText=True)
+    # 设置坐标轴范围
+    max_X = np.max(x)
+    max_Y = np.max(y)
+    plt.xlim((0, max_X))
+    plt.ylim((0, max_Y))
+
+    plt.xlabel("data's length")
+    plt.ylabel("transport latency(ms)")
+
+    # 设置坐标轴刻度
+    # my_x_ticks = np.arange(0, max_X)
+    # my_y_ticks = np.arange(0, max_Y)
+    # plt.xticks(my_x_ticks)
+    # plt.yticks(my_y_ticks)
+
+    plt.show()
+
+
 if __name__ == '__main__':
     path = "../res/transport_time.xls"
     sheet_name = "time1"
@@ -18,26 +44,6 @@ if __name__ == '__main__':
 
     x = np.array(data_length)
     y = np.array(transport_time)
-    s = 50
 
-    fig = plt.figure(figsize=(8,5))
+    getScatterImg(x,y)
 
-    plt.scatter(x, y, s, c="g", alpha=0.5)
-    plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0), useMathText=True)
-    plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0), useMathText=True)
-    # 设置坐标轴范围
-    max_X = np.max(x)
-    max_Y = np.max(y)
-    plt.xlim((0, max_X))
-    plt.ylim((0, max_Y))
-
-    plt.xlabel("shape's prod")
-    plt.ylabel("data's length")
-
-    # 设置坐标轴刻度
-    # my_x_ticks = np.arange(0, max_X)
-    # my_y_ticks = np.arange(0, max_Y)
-    # plt.xticks(my_x_ticks)
-    # plt.yticks(my_y_ticks)
-
-    plt.show()
