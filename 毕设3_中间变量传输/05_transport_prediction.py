ab148171a2768d4f6ea98adc460804343768d48e
tjyy <1123351509@qq.com>
2022-04-16 22:35:44 +0800

    ploynomial regression failed

--- 毕设3_中间变量传输/05_transport_prediction.py
+++ 毕设3_中间变量传输/05_transport_prediction.py
@@ -5,6 +5,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
+from sklearn.preprocessing import PolynomialFeatures
 
 def getScatterImg(x,y):
     s = 50
@@ -32,15 +33,19 @@ def getScatterImg(x,y):
     plt.show()
 
 
-def getImgWithLine(x,y,coef,intercept):
+def getLinearImg(x,y,coef,intercept):
+    """
+    :param coef:  16.789
+    :param intercept: 425.863
+    """
     s = 50
-
     fig = plt.figure(figsize=(8, 5))
 
     plt.scatter(x, y, s, c="g", alpha=0.5)
     plt.plot(x,coef[0] * x + intercept)
     plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0), useMathText=True)
     plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0), useMathText=True)
+
     # 设置坐标轴范围
     max_X = np.max(x)
     max_Y = np.max(y)
@@ -50,13 +55,11 @@ def getImgWithLine(x,y,coef,intercept):
     plt.xlabel("data's length")
     plt.ylabel("transport latency(ms)")
 
-
     plt.show()
+
 
 def myLinearRegression(X,y):
     X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=1)
-    # print(X_train.shape)
-    # print(y_train.shape)
 
     linreg = LinearRegression()
     linreg.fit(X_train,y_train)
@@ -65,6 +68,24 @@ def myLinearRegression(X,y):
     print("test RMSE:",metrics.mean_squared_error(y_pred,y_test))
 
     return linreg.coef_,linreg.intercept_
+
+
+def myPolynomialRegression(X,y):
+    poly = PolynomialFeatures(degree=3)
+    poly.fit(X)
+    X2 = poly.transform(X)
+
+    lin_reg = LinearRegression()
+    lin_reg.fit(X2, y)
+    y_predict2 = lin_reg.predict(X2)
+
+    plt.scatter(x, y)
+    plt.plot(np.sort(x), y_predict2[np.argsort(x)], color='r')
+
+    plt.xlabel("data's length")
+    plt.ylabel("transport latency(ms)")
+
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -78,14 +99,15 @@ if __name__ == '__main__':
     transport_time = function.get_excel_data(path,sheet_name,"transport time(ms)")
 
     x = np.array(shape_prod)
-    y = np.array(data_length)
+    y = np.array(transport_time)
+    x.resize((*x.shape, 1))
 
 
-    x.resize((*x.shape,1))
-    coef,intercept = myLinearRegression(x,y)
-    getImgWithLine(x,y,coef,intercept)
+    """ 线性回归 """
+    # coef,intercept = myLinearRegression(x,y)
+    # getLinearImg(x,y,coef,intercept)
 
-
+    myPolynomialRegression(x,y)
 
 
 
