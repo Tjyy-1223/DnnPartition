ea956d2d1cd652dfd19f0d0a0b00b6beb422312e
tjyy <1123351509@qq.com>
2022-04-17 12:55:07 +0800

    data's shape - transport time 回归预测完成

--- 毕设3_中间变量传输/05_transport_prediction.py
+++ 毕设3_中间变量传输/05_transport_prediction.py
@@ -58,34 +58,90 @@ def getLinearImg(x,y,coef,intercept):
     plt.show()
 
 
-def myLinearRegression(X,y):
-    X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=1)
+def myLinearRegression(x,y):
+    """
+       coef:  16.789
+       intercept: 425.863
+    """
+    X = x.reshape(-1,1)
 
     linreg = LinearRegression()
-    linreg.fit(X_train,y_train)
+    linreg.fit(X,y)
 
-    y_pred = linreg.predict(X_test)
-    print("test RMSE:",metrics.mean_squared_error(y_pred,y_test))
+    y_pred = linreg.predict(X)
+    print("test MSE:",metrics.mean_squared_error(y_pred,y))
 
-    return linreg.coef_,linreg.intercept_
+    s = 50
+
+    fig = plt.figure(figsize=(8, 5))
+    plt.scatter(x, y, s, c="g", alpha=0.5)
+    plt.plot(np.sort(x), y_pred[np.argsort(x)])
+
+    plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0), useMathText=True)
+    plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0), useMathText=True)
+
+    # 设置坐标轴范围
+    max_X = np.max(x)
+    max_Y = np.max(y)
+    plt.xlim((0, max_X))
+    plt.ylim((0, max_Y))
+
+    plt.xlabel("data's shape")
+    plt.ylabel("data's length")
+
+    plt.show()
 
 
-def myPolynomialRegression(X,y):
-    poly = PolynomialFeatures(degree=3)
+def myTransform(x,degree = 1):
+    X = x.reshape(-1, 1)
+
+    poly = PolynomialFeatures(degree=degree)
     poly.fit(X)
     X2 = poly.transform(X)
 
+    return X2
+
+
+def myPolynomialRegression(x,y):
+    s = 50
+
+    X2 = myTransform(x,degree=3)
+
     lin_reg = LinearRegression()
     lin_reg.fit(X2, y)
-    y_predict2 = lin_reg.predict(X2)
 
-    plt.scatter(x, y)
-    plt.plot(np.sort(x), y_predict2[np.argsort(x)], color='r')
+    y_predict = lin_reg.predict(X2)
+    print("test MSE:", metrics.mean_squared_error(y_predict, y))
+    # print("斜率参数: ",lin_reg.coef_)
+    # print("截距参数: ",lin_reg.intercept_)
+    """
+    degree = 2
+    斜率参数: [0.00000000e+00 2.11090193e-04 5.44408375e-10]
+    截距参数: 40.11139070205547
+    
+    degree = 3
+    斜率参数:  [0.00000000e+00 4.87227671e-04 2.36099771e-10 8.03935911e-17]
+    截距参数:  6.9646173709356844
+    """
 
-    plt.xlabel("data's length")
+    plt.scatter(x, y, s, c="g", alpha=0.5)
+    plt.scatter(x,y_predict)
+    plt.plot(np.sort(x), y_predict[np.argsort(x)], color='r')
+
+    plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0), useMathText=True)
+    plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0), useMathText=True)
+
+    # 设置坐标轴范围
+    max_X = np.max(x)
+    max_Y = np.max(y)
+    plt.xlim((0, max_X))
+    plt.ylim((0, max_Y))
+
+    plt.xlabel("data's shape")
     plt.ylabel("transport latency(ms)")
 
     plt.show()
+    return lin_reg
 
 
 if __name__ == '__main__':
@@ -100,15 +156,17 @@ if __name__ == '__main__':
 
     x = np.array(shape_prod)
     y = np.array(transport_time)
-    x.resize((*x.shape, 1))
-
 
     """ 线性回归 """
-    # coef,intercept = myLinearRegression(x,y)
-    # getLinearImg(x,y,coef,intercept)
-
-    myPolynomialRegression(x,y)
+    # myLinearRegression(x,y)
 
 
+    lin_reg = myPolynomialRegression(x,y)
+    # print(lin_reg.coef_)
+
+    x = [28224,44100,63504,86436,112896,142884,176400,213444,254016,298116,345744,396900,451584]
+    x = np.array(x)
+    X = myTransform(x,degree=3)
+    print(lin_reg.predict(X))
 
 
