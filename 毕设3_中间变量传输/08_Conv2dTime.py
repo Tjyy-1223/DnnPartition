031bd9bcf5dde62fe0151cedad0aa5b0024b2ea2
tjyy <1123351509@qq.com>
2022-04-19 11:02:42 +0800

    data update

---
+++ 毕设3_中间变量传输/08_Conv2dTime.py
@@ -0,0 +1,15 @@
+import torch
+import function
+import torch.nn as nn
+
+
+
+
+if __name__ == '__main__':
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    epoch = 300
+
+    x = torch.rand(size=(1,64,112,112))
+    x = x.to(device)
+
+    
