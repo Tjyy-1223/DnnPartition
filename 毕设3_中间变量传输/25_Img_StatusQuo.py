773ff7e878809d660b71d8face9cdd4cd6795dad
tjyy <1123351509@qq.com>
2022-05-01 19:42:16 +0800

    img for part 3

--- 毕设3_中间变量传输/25_Img_StatusQuo.py
+++ 毕设3_中间变量传输/25_Img_StatusQuo.py
@@ -19,35 +19,46 @@ if __name__ == '__main__':
     # print(times)
 
 
-    ind = np.arange(len(index))  # the x locations for the groups
-    width = 0.35  # the width of the bars
+    ind = np.arange(4)  # the x locations for the groups
+    width = 0.33  # the width of the bars
 
-    fig = plt.figure(figsize=(8,3.5))
-    ax1 = fig.add_subplot(111)
+    # fig = plt.figure(figsize=(8,3.5))
+
+    edge_value = [0.001,25.09,13.528]
+    transmission_value = [119.166,8.159,0.18]
+    cloud_value = [2.856,0.002,1.582]
+    end_value = [122.033,25.272,23.269]
+
+    cloud_compute = [0.001,119.166,2.856,122.033]
+    edge_compute = [25.09,0.18,0.002,25.272]
+    end_to_end_compute = [13.528,8.159,1.582,23.269]
+
+
+    fig, ax1 = plt.subplots(figsize=(7,3))
+    # ax1 = fig.add_subplot(111)
     ax1.xaxis.set_ticks_position('bottom')
     ax1.set_xticks(ind)
-    ax1.set_xticklabels(('conv1', 'maxPool2d1', 'conv2', 'maxPool2d2', 'conv3',
-                     'conv4', 'conv5', 'maxPool2d3', 'avgPool2d', 'flatten',
-                     'linear1', 'linear2', 'linear3'),rotation = -45)
-    lns1 = ax1.bar(ind - width / 2, times, width, label='latency')
+    ax1.set_xticklabels(('Edge','Transmission','Cloud','End-to-end'),rotation = -15)
+    lns1 = ax1.bar(ind - width / 2, cloud_compute,width/2,label='Cloud-Only')
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
     # ax.set_ylabel('Scores')
 
-    ax2 = ax1.twinx()
-    ax2.set_xticks(ind)
-    ax2.set_xticklabels(('conv1', 'maxPool2d1', 'conv2', 'maxPool2d2', 'conv3',
-                         'conv4', 'conv5', 'maxPool2d3', 'avgPool2d', 'flatten',
-                         'linear1', 'linear2', 'linear3'), rotation=-45)
-    lns2 = ax2.bar(ind + width / 2, transport_num, width, color='darkblue', label='shape')
+    # ax2 = ax1.twinx()
+    # ax2.set_xticks(ind)
+    # ax2.set_xticklabels(('conv1', 'maxPool2d1', 'conv2', 'maxPool2d2', 'conv3',
+    #                      'conv4', 'conv5', 'maxPool2d3', 'avgPool2d', 'flatten',
+    #                      'linear1', 'linear2', 'linear3'), rotation=-45)
+    lns2 = ax1.bar(ind , edge_compute, width/2, color='darkblue', label='Edge-Only')
+    ax1.bar(ind+ width / 2,end_to_end_compute,width/2,color='darkcyan',label='Edge-Cloud')
+    # ax2.set_title('Latency And Shape of AlexNet')
 
-    ax2.set_title('Latency And Shape of AlexNet')
-
-    fig.legend(loc=1, bbox_to_anchor=(1,1), bbox_transform=ax1.transAxes)
+    # fig.legend(loc=1, bbox_to_anchor=(1,1), bbox_transform=ax1.transAxes)
 
     ax1.set_ylabel('Latency(ms)')
-    ax2.set_ylabel('Shape')
+    # ax2.set_ylabel('Shape')
 
     plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0), useMathText=True)
+    plt.legend()
     plt.tight_layout()
     plt.show()
